e116238531d14106208fe157a6326024f943a22b
Wesley Pedreira Passos Rocha <wesley.p.rocha6@aluno.senai.br>
2026-02-27 22:10:24 -0300

    simplificação de renderização na tela e remoção de controle de índice manualD

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ let formularioDeCadastrarTreino = document.querySelector('.formularioParaCadastr
 
 let treinos = [];
 const campo = document.querySelector('.treinosCadastrados');
-let cont = 0;
 
 formularioDeCadastrarTreino.addEventListener('submit', function (event) {
     event.preventDefault();
     treinos.push(event.target.nome.value);
-    mostrarTreino();
     console.log('Formulário enviado', treinos);
-    event.target.nome.value = '';
-});
+    event.target.nome.value = ''; 
+    MostrarTreino();
+})
 
-function mostrarTreino() {
+function MostrarTreino() {
+    campo.innerHTML = '';
+    
+    treinos.forEach((treino, index) => {
+        console.log('index no valor: ' + index)
 
-    for (let index = cont; index < treinos.length; index++) {
         const li = document.createElement('li');
         const botaoEditarTreino = document.createElement('button');
         const botaoExcluirTreino = document.createElement('button');
@@ -30,22 +32,15 @@ function mostrarTreino() {
         // Deletar treino
         botaoExcluirTreino.addEventListener('click', function () {
             treinos.splice(index, 1);
-            cont = 0;
-            campo.textContent = '';
-            li.textContent = '';
-            mostrarTreino();
+            index = index - 1;
+            MostrarTreino(); 
         });
 
         // Editar treino
         botaoEditarTreino.addEventListener('click', function () {
-            treinos[index] = prompt('Qual é o novo nome?', treinos[index]);
-            cont = 0;
-            campo.textContent = '';
-            li.textContent = '';
-            mostrarTreino();
+            treinos[index] = prompt('Qual é o novo nome?', treino);
+            MostrarTreino(); 
         });
-
     }
-    // se treinos == 0 o cont vai valer zero se não a contagem continua
-    treinos.length === 0 ? cont = 0 : cont = cont + 1;
+    );
 }
